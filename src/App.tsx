08fb86e6522244d07dcdf42a6959a24553ab91f9
Floria Gu <gfzting@gmail.com>
2023-03-11 14:01:38 -0800

    Add full-screen Spline scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Spline from "@splinetool/react-spline";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Spline
+        style={{
+          width: "100vw",
+          height: "100vh"
+        }}
+        scene={"https://prod.spline.design/cyaFi9uSHDHBGYVv/scene.splinecode"}
+      />
     </div>
   );
 }
