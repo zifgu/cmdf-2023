20924d55380d8c3b2c8f82233497219bd852f5ca
YuriLeeee <71787363+YuriLeeee@users.noreply.github.com>
2023-03-11 22:00:36 -0800

    modified home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,23 +11,34 @@ export function Home() {
                 <link rel="Home" href='Home.css'/>
             </head>
             <div className='intro'>
-                <h1>HOME</h1>
-                
+                <h1>
+                    <span className="u-text-uppercase u-text-color-md-green-900 u-font-weight-900">Z</span>
+                    <span className="u-text-lowercase u-text-color-md-white-800 u-font-weight-900">en</span>
+                    <span className="u-text-uppercase u-text-color-md-green-900 u-font-weight-900">Z</span>
+                    <span className="u-text-lowercase u-text-color-md-white-800 u-font-weight-900">one</span>
+                    <span className="u-text-lowercase u-text-color-md-white-800 u-font-weight-900"> 3D</span>
+                </h1>
+                <p>What you need to hear</p>
+                <div className='vertical-center'>
+                    <a href='/game'>
+                            <button>Play</button>
+                    </a>
+                    <a href='/game'>
+                            <button>Option</button>
+                    </a>
+                    <a href='/game'>
+                            <button>Credits</button>
+                    </a>
+                </div>
+
                 &nbsp;
                 <p>Hi there, welcome to our home page! You are welcome to navigate the island and participate in conversations
                     with an AI in each of the island that gives personalized affirmations based on your problems!
                 </p>
                 &nbsp;
-                <p>Island Description</p>
-                &nbsp;
-                <p>
-                    If you are ready, click the button below and start your game!
-                </p>
+                
+
                 <div className='item'>
-                    <a href='/game'>
-                        <button>Start Game</button>
-                    </a>
-                    &nbsp;
                     <img id="pig" src="img.png" width="100" height="100" />
                 </div>
                 
