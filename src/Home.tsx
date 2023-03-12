8180725256eb91aaea9ca9eef4ea5c39390053ef
YuriLeeee <71787363+YuriLeeee@users.noreply.github.com>
2023-03-11 20:41:59 -0800

    modified home page

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Spline from "@splinetool/react-spline";
 import './Home.css';
+//import img from './img.png';
 
 export function Home() {
     return (
@@ -11,16 +12,32 @@ export function Home() {
             </head>
             <div className='intro'>
                 <h1>HOME</h1>
+                
                 &nbsp;
-                <p>Hi there! Welcome to our home page! You are welcome to navigate through 3 islands and participate in conversations
+                <p>Hi there, welcome to our home page! You are welcome to navigate the island and participate in conversations
                     with an AI in each of the island that gives personalized affirmations based on your problems!
                 </p>
+                &nbsp;
+                <p>Island Description</p>
+                &nbsp;
+                <p>
+                    If you are ready, click the button below and start your game!
+                </p>
+                <div className='item'>
+                    <a href='/game'>
+                        <button>Start Game</button>
+                    </a>
+                    &nbsp;
+                    <img id="pig" src="img.png" width="100" height="100" />
+                </div>
+                
             </div>
-            <div className='island'>
+            
+            {/* <div className='island'>
                 <h3>Beach</h3>
                 <h3>Forest</h3>
                 <h3>Ubc</h3>
-            </div>
+            </div> */}
 
         </div>
     );
